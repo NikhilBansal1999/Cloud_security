37a7177afdb52566b6ba450fa44703f573bbff38
NIKHIL <nikhil.accent@gmail.com>
2018-12-14 00:26:41 +0000

    Added Error checks

--- load_lib.c
+++ load_lib.c
@@ -17,6 +17,23 @@ typedef uint64_t Elf64_Off;
 
 #define EI_NIDENT 16
 
+#define PT_DYNAMIC 2
+#define PT_PHDR 6
+#define PT_LOAD 1
+#define PT_GNU_STACK 0x6474e551
+
+#define SHT_SYMTAB 2
+#define SHT_STRTAB 3
+#define SHT_DYNAMIC 6
+
+#define DT_PLTRELSZ 2
+#define DT_SYMTAB 6
+#define DT_RELA 7
+#define DT_RELASZ 8
+#define DT_SYMENT 11
+#define DT_INIT 12
+#define DT_JMPREL 23
+
 typedef struct
 {
   FILE* file_d;          /*File descriptor of open library*/
@@ -117,12 +134,14 @@ typedef struct
 #define ELF64_R_TYPE(info)            ((Elf64_Word)(info))
 #define ELF64_R_INFO(sym, type)       (((Elf64_Xword)(sym)<<32)+(Elf64_Xword)(type))
 
+/* Truncates the passed number to the nearest multiple of size*/
 long align_down(long addr, long size)
 {
   long ans=addr-(addr%size);
   return ans;
 }
 
+/*Round up the given number to the next multiple of size*/
 long align_up(long addr,long size)
 {
   if(addr%size==0)
@@ -136,15 +155,25 @@ long align_up(long addr,long size)
   }
 }
 
+/*Maps library into memory and returns a handle to the mapped region*/
 link_info* map_library(char* lib_name)
 {
   Elf_header* header=(Elf_header*)calloc(1,sizeof(Elf_header));
   link_info* info=(link_info*)calloc(1,sizeof(link_info));
   long pagesize = sysconf(_SC_PAGESIZE);
   FILE* fd=fopen(lib_name,"r");
+  if(fd==NULL)
+  {
+    printf("Cannot open the shared library file!\n");
+    return NULL;
+  }
 
   info->file_d=fd;
   int data_read=fread(header,sizeof(Elf_header),1,fd);
+  if(data_read != 1)
+  {
+    printf("Error reading Elf Header!\n");
+  }
   if(header->e_type != 3)
   {
     printf("The given file is not a shared object\n");
@@ -153,35 +182,37 @@ link_info* map_library(char* lib_name)
   info->entry=header->e_entry;
   info->phnum=header->e_phnum;
   Elf64_Program_header prog_heads[header->e_phnum];
-  fseek(fd,header->e_phoff,SEEK_SET);
+  if(fseek(fd,header->e_phoff,SEEK_SET) == -1)
+  {
+    printf("Error parsing file\n");
+  }
   data_read=fread(prog_heads,sizeof(Elf64_Program_header),header->e_phnum,fd);
+  if(data_read != header->e_phnum)
+  {
+    printf("Error reading program Header table\n");
+  }
 
   command commands[info->phnum];
   int num_commands=0;
-  int gap=0;
   for(int i=0;i<header->e_phnum;i++)
   {
-    if(prog_heads[i].p_type==2)  /*Header type PT_DYNAMIC*/
+    if(prog_heads[i].p_type==PT_DYNAMIC)  /*Header type PT_DYNAMIC*/
     {
       info->dyn_vaddr=prog_heads[i].p_vaddr;
       info->dyn_num_ents=prog_heads[i].p_memsz/sizeof(Elf64_Dyn);
       info->dyn_num=i+1;
     }
-    if(prog_heads[i].p_type==6)  /*Header Type PT_Phdr*/
+    if(prog_heads[i].p_type==PT_PHDR)  /*Header Type PT_Phdr*/
     {
       info->pht_vaddr=prog_heads[i].p_vaddr;
     }
-    if(prog_heads[i].p_type==1)  /*Header Type PT_Load*/
+    if(prog_heads[i].p_type==PT_LOAD)  /*Header Type PT_Load*/
     {
       commands[num_commands].mapstart = align_down(prog_heads[i].p_vaddr,pagesize);
   	  commands[num_commands].mapend = align_up(prog_heads[i].p_vaddr + prog_heads[i].p_filesz,pagesize);
   	  commands[num_commands].dataend = prog_heads[i].p_vaddr + prog_heads[i].p_filesz;
   	  commands[num_commands].allocend = prog_heads[i].p_vaddr + prog_heads[i].p_memsz;
   	  commands[num_commands].mapoff = align_down(prog_heads[i].p_offset,pagesize);
-      if((num_commands>1) && (commands[num_commands].mapstart != commands[num_commands-1].mapend))
-      {
-        gap=1;
-      }
       commands[num_commands].prot=0;
       if(prog_heads[i].p_flags & 4)/*Give read permissions*/
       {
@@ -197,20 +228,31 @@ link_info* map_library(char* lib_name)
       }
       num_commands=num_commands+1;
     }
-    if(prog_heads[i].p_type==0x6474e551) /*Header Type PT_GNU_STACK*/
+    if(prog_heads[i].p_type==PT_GNU_STACK) /*Header Type PT_GNU_STACK*/
     {
       info->stack_state=prog_heads[i].p_flags;
     }
    /*PT_NOTE and PT_TLS left out*/
   }
-
   /*Start mapping of library*/
   int fd_new=open(lib_name, O_RDONLY | O_NOCTTY); //using fopen gave errors
+  if(fd_new == -1)
+  {
+    printf("Error opening file\n");
+  }
   long length_of_mapping=commands[num_commands-1].allocend-commands[0].mapstart;
   info->start_of_mapping=(Elf64_Addr)mmap(NULL,length_of_mapping,commands[0].prot,MAP_PRIVATE|MAP_FILE,fd_new,commands[0].mapoff);
+  if(info->start_of_mapping == -1)
+  {
+    printf("Mmap failed\n");
+  }
   info->end_of_mapping=info->start_of_mapping+length_of_mapping;
   info->base_addr=info->start_of_mapping-commands[0].mapstart;
   int err_val=mprotect((void *)(commands[0].mapend+info->base_addr),commands[num_commands-1].allocend-commands[0].mapend,PROT_NONE);
+  if(err_val == -1)
+  {
+    printf("Setting memory protections failed\n");
+  }
   if(commands[0].allocend>commands[0].dataend)
   {
     memset((void *)(commands[0].dataend+info->base_addr),'\0',(commands[0].allocend-commands[0].dataend));
@@ -219,10 +261,18 @@ link_info* map_library(char* lib_name)
   {
     length_of_mapping=commands[i].mapend-commands[i].mapstart;
     void *temp_point=mmap((void *)(info->base_addr+commands[i].mapstart),length_of_mapping,commands[i].prot,MAP_PRIVATE|MAP_FILE|MAP_FIXED,fd_new,commands[i].mapoff);
+    if(temp_point == -1)
+    {
+      printf("Mmap failed\n");
+    }
     if(commands[i].allocend>commands[i].dataend)
     {
       memset((void *)(commands[i].dataend+info->base_addr),'\0',(commands[i].allocend-commands[i].dataend));
     }
+  }
+  if(close(fd_new) == -1)
+  {
+    printf("Error closing file\n");
   }
   if(info->dyn_vaddr != NULL)
   {
@@ -233,23 +283,29 @@ link_info* map_library(char* lib_name)
     info->pht_vaddr = info->pht_vaddr + info->base_addr;
   }
   Elf_Section_header section[header->e_shnum];
-  fseek(fd,header->e_shoff,SEEK_SET);
+  if(fseek(fd,header->e_shoff,SEEK_SET) == -1)
+  {
+    printf("Error parsing file\n");
+  }
   data_read=fread(section,sizeof(Elf_Section_header),header->e_shnum,fd);
-
+  if(data_read != header->e_shnum)
+  {
+    printf("Error reading section header\n");
+  }
   Elf64_Addr dynamic;
   int num_dyn_ent;
   for(int i=0;i < header->e_shnum ;i++)
   {
-    if(section[i].sh_type==2)   /*Symbol Table entry*/
+    if(section[i].sh_type==SHT_SYMTAB)   /*Symbol Table entry*/
     {
       info->symbol_table = section[i].sh_offset;
       info->num_sym_entry = section[i].sh_size/section[i].sh_entsize;
     }
-    if(section[i].sh_type==3)   /*String Table entry*/
+    if(section[i].sh_type==SHT_STRTAB)   /*String Table entry*/
     {
       info->string_table = section[i].sh_offset;
     }
-    if(section[i].sh_type==6)  /* DYNAMIC Section */
+    if(section[i].sh_type==SHT_DYNAMIC)  /* DYNAMIC Section */
     {
       dynamic=section[i].sh_offset;
       num_dyn_ent=section[i].sh_size/section[i].sh_entsize;
@@ -258,8 +314,15 @@ link_info* map_library(char* lib_name)
   Elf64_Dyn dyn_entries[num_dyn_ent];
   Elf64_Addr relocation_addr;
   int num_relocations;
-  fseek(fd,dynamic,SEEK_SET);
+  if(fseek(fd,dynamic,SEEK_SET) == -1)
+  {
+    printf("Error paring file\n");
+  }
   data_read=fread(dyn_entries,sizeof(Elf64_Dyn),num_dyn_ent,fd);
+  if(data_read != num_dyn_ent)
+  {
+    printf("Error reading file\n");
+  }
   void (*init)();
 
   Elf64_Addr dyn_sym_offset;
@@ -269,31 +332,31 @@ link_info* map_library(char* lib_name)
   Elf64_Addr plt_offset;
   for(int i=0;i<num_dyn_ent;i++)
   {
-    if(dyn_entries[i].d_tag==6) /*Symbol Table*/
+    if(dyn_entries[i].d_tag==DT_SYMTAB) /*Symbol Table*/
     {
       dyn_sym_offset=dyn_entries[i].d_un.d_ptr;
     }
-    if(dyn_entries[i].d_tag==11) /*Size of symbol table*/
+    if(dyn_entries[i].d_tag==DT_SYMENT) /*Size of symbol table*/
     {
       sym_tabsize=dyn_entries[i].d_un.d_val;
     }
-    if(dyn_entries[i].d_tag==7) /* DT_RELA*/
+    if(dyn_entries[i].d_tag==DT_RELA) /* DT_RELA*/
     {
       relocation_addr=dyn_entries[i].d_un.d_ptr;
     }
-    if(dyn_entries[i].d_tag==8) /*DT_RELASZ*/
+    if(dyn_entries[i].d_tag==DT_RELASZ) /*DT_RELASZ*/
     {
       num_relocations=dyn_entries[i].d_un.d_val/sizeof(Elf64_Rela);
     }
-    if(dyn_entries[i].d_tag==12)  /*DT_INIT*/
+    if(dyn_entries[i].d_tag==DT_INIT)  /*DT_INIT*/
     {
       init = info->base_addr+dyn_entries[i].d_un.d_ptr;
     }
-    if(dyn_entries[i].d_tag==2)  /*Size of relocation entries associated with PLT*/
+    if(dyn_entries[i].d_tag==DT_PLTRELSZ)  /*Size of relocation entries associated with PLT*/
     {
       plt_ents=dyn_entries[i].d_un.d_val;
     }
-    if(dyn_entries[i].d_tag==23) /*DT_JMPREL*/
+    if(dyn_entries[i].d_tag==DT_JMPREL) /*DT_JMPREL*/
     {
       plt_offset=dyn_entries[i].d_un.d_ptr;
     }
@@ -302,12 +365,26 @@ link_info* map_library(char* lib_name)
 
   dyn_sym_num=sym_tabsize/sizeof(Elf_Symtab_ent);
   Elf_Symtab_ent symbols[dyn_sym_num];
-  fseek(info->file_d,dyn_sym_offset,SEEK_SET);
-  int data_red=fread(symbols,sizeof(Elf_Symtab_ent),info->num_sym_entry,info->file_d);
+  if(fseek(info->file_d,dyn_sym_offset,SEEK_SET) == -1)
+  {
+    printf("Error parsing file\n");
+  }
+  data_read=fread(symbols,sizeof(Elf_Symtab_ent),info->num_sym_entry,info->file_d);
+  if(data_read != info->num_sym_entry)
+  {
+    printf("Error reading file\n");
+  }
 
   Elf64_Rela relocations[num_relocations];
-  fseek(fd,relocation_addr,SEEK_SET);
-  fread(relocations,sizeof(Elf64_Rela),num_relocations,fd);
+  if(fseek(fd,relocation_addr,SEEK_SET) == -1)
+  {
+    printf("Error parsing file\n");
+  }
+  data_read=fread(relocations,sizeof(Elf64_Rela),num_relocations,fd);
+  if(data_read != num_relocations)
+  {
+    printf("Error reading file\n");
+  }
   for(int i=0;i<num_relocations;i++)
   {
     int sym_index=ELF64_R_SYM(relocations[i].r_info);
@@ -323,8 +400,15 @@ link_info* map_library(char* lib_name)
     }
   }
   Elf64_Rela plt_relocations[plt_ents];
-  fseek(fd,plt_offset,SEEK_SET);
+  if(fseek(fd,plt_offset,SEEK_SET) == -1)
+  {
+    printf("Error parsing file\n");
+  }
   data_read=fread(plt_relocations,sizeof(Elf64_Rela),plt_ents,fd);
+  if(data_read != plt_ents)
+  {
+    printf("Error reading file\n");
+  }
   for(int i=0;i<plt_ents;i++)
   {
     int sym_index=ELF64_R_SYM(plt_relocations[i].r_info);
@@ -366,9 +450,15 @@ link_info* map_library(char* lib_name)
 void * get_function(link_info* info,char *func_name)
 {
   Elf_Symtab_ent symbols[info->num_sym_entry];
-  fseek(info->file_d,info->symbol_table,SEEK_SET);
-  int data_red=fread(symbols,sizeof(Elf_Symtab_ent),info->num_sym_entry,info->file_d);
-
+  if(fseek(info->file_d,info->symbol_table,SEEK_SET) == -1)
+  {
+    printf("Error parsing library\n");
+  }
+  int data_read=fread(symbols,sizeof(Elf_Symtab_ent),info->num_sym_entry,info->file_d);
+  if(data_read != info->num_sym_entry)
+  {
+    printf("Error parsing library\n");
+  }
   char str[strlen(func_name)+1];
   for(int i=0;i < info->num_sym_entry;i++)
   {
@@ -380,12 +470,24 @@ void * get_function(link_info* info,char *func_name)
       return addr;
     }
   }
+  return NULL;
 }
 int main(int argc, char* argv[])
 {
   link_info* handle=map_library("./lib_test.so");
+  if(handle == NULL)
+  {
+    printf("Error opening library\n");
+  }
   int (*fibo)(int);
   fibo = (int (*)(int))get_function(handle, "fibonacci");
-  printf("%d\n",(*fibo)(atoi(argv[1])));
+  if(fibo == NULL)
+  {
+    printf("Function not found");
+  }
+  else
+  {
+    printf("%d\n",(*fibo)(atoi(argv[1])));
+  }
   return 0;
 }
